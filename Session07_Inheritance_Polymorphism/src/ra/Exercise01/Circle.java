27035f9e92eea7dd98155bd21aacb55ea36df8f0
xuanhong258 <xuanhong258@.com>
2024-05-23 17:50:42 +0700

    Add Session07

---
+++ Session07_Inheritance_Polymorphism/src/ra/Exercise01/Circle.java
@@ -0,0 +1,48 @@
+package ra.Exercise01;
+
+public class Circle {
+    private float radius;
+    private String color;
+    private float area;
+    final float PI = 3.14F;
+    public Circle() {
+    }
+
+    public Circle(float radius, String color, float area) {
+        this.radius = radius;
+        this.color = color;
+        this.area = area;
+    }
+
+    public float getRadius() {
+        return radius;
+    }
+
+    public void setRadius(float radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public float getArea() {
+        return area;
+    }
+
+    public void setArea(float area) {
+        this.area = area;
+    }
+
+    public void calArea(){
+        this.area = this.radius * this.radius * PI;
+    }
+
+    public String toString(){
+        return "Radius: " + this.radius + " - Color: " + this.color + " - Area: " + this.area;
+    }
+}
---
+++ Session07_Inheritance_Polymorphism/src/ra/Exercise01/Cylinder.java
@@ -0,0 +1,41 @@
+package ra.Exercise01;
+
+public class Cylinder extends Circle{
+    private float height;
+
+    public Cylinder() {
+    }
+
+    public Cylinder(float radius, String color, float area, float height) {
+        super(radius, color, area);
+        this.height = height;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float calVolume(){
+        return super.getArea() * this.height;
+    }
+
+    public String toString(){
+        return "Radius: " + super.getRadius() + " - Color: " + super.getColor() + " - Area: " +
+                super.getArea() + " - Height: " + this.height + " - Volume: " + this.calVolume();
+    }
+
+    public static void main(String[] args) {
+        Cylinder cylinder = new Cylinder();
+
+        cylinder.setRadius(5);
+        cylinder.setColor("Blue");
+        cylinder.calArea();
+        cylinder.setHeight(10);
+
+        System.out.println(cylinder);
+    }
+}
